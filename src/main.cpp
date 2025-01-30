31193561f8b09c196c6d975a181169f9df7c58a2
mike <miketetteh751@outlook.com>
2025-01-30 18:39:56 +0000

    dependencies configured

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+#include <libswresample/swresample.h>
+#include <libavutil/imgutils.h>
+}
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
